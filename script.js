6ecfde258e389a0b49fdc0954bfa88adc454e0a5
Sayeedakh <144889961+Sayeedakh@users.noreply.github.com>
2023-09-13 14:01:28 +0530

    iraxatech
    1st web

---
+++ script.js
@@ -0,0 +1,30 @@
+
+// function sendMail() {
+//     var params = {
+//         name: document.getElementById("name").value,
+//         email: document.getElementById("email").value,
+//         phone: document.getElementById("phone").value,
+//         message: document.getElementById("message").value,
+//     };
+// const ServiceID = "service_5rralyg";
+// const tempelateID = "template_9f9udc8";
+
+// emailjs.send(ServiceID, tempelateID, params)
+// .then((res) => {
+//     document.getElementById("name").value="",
+//     document.getElementById("email").value="",
+//     document.getElementById("phone").value="",
+//     document.getElementById("Message").value="",
+
+//     console.log(res);
+//     alert("Your message sent successfully")
+
+// })
+
+// .catch((err) => console.log(err));
+
+
+
+// }
+
+
